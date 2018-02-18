f9df87341b2661019c8f3bf196d756542b1a0d97
KarlAbramCrespo <32053986+KarlAbramCrespo@users.noreply.github.com>
2018-02-18 14:23:18 +0800

    Added Login Window
    Added front end for login

---
+++ login.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root = Tk()
+
+titleMessage = Label(root, text="Enter Username")
+textInput = Entry(root)
+textSubmit = Button(root, text = "Login")
+
+titleMessage.grid(row=0)
+textInput.grid(row=1, sticky=W+E)
+textSubmit.grid(row=2, sticky=E+W)
+
+
+root.mainloop()
