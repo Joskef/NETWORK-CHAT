2a29a06b83bca88793c67eba9d53f838976ec303
KarlAbramCrespoDLSU <karl_crespo@dlsu.edu.ph>
2018-03-17 22:20:24 +0800

    Working Private Message on new GUI and Added File Transfer Backend
    Issue with Group chat being able to send messages on Private Chat
    channels.

--- chatClientMP2.py
+++ chatClientMP2.py
@@ -46,8 +46,7 @@ class LoginWindow:
             tempUserString = self.textInput.get()
             self.textFeedback.config(text="Successfully logged in!", fg="dark green")
             self.newWindow = Toplevel(self.master)
-            self.client = MainWindow(self.newWindow,
-                                     tempUserString)  # create new window and pass the new username string to the new window
+            self.client = MainWindow(self.newWindow, tempUserString)  # create new window and pass the new username string to the new window
 
 
 class MainWindow:
@@ -56,6 +55,7 @@ class MainWindow:
         frame = Frame(master)
         frame.pack()
         self.usernameString = usernameString
+        self.chatRecipientNum = 0;
 
         # GUI initialization
 
@@ -68,6 +68,8 @@ class MainWindow:
         self.logoutBtn = Button(frame, text="Logout")
         self.createChatRoomBtn = Button(frame, text="Create Chat Room")
         self.createGroupChatBtn = Button(frame, text="Create Group Chat")
+        self.enterGlobalChatBtn = Button(frame, text="Enter Global Chat Mode")
+        self.enterPrivateChatBtn = Button(frame, text="Enter Private Chat Mode")
 
         # For Users list
         self.scrollbarUsers = Scrollbar(frame)
@@ -84,17 +86,23 @@ class MainWindow:
         self.listboxChatrooms.grid(row=3, column=0)
         self.scrollbarChatrooms.grid(row=3, column=1, sticky=N + S)
         self.welcomeUserMessage.grid(row=0, column=3)
-        self.chatBox.grid(row=1, column=3, rowspan=3, columnspan=2, sticky=N + S)
-        self.chatInput.grid(row=5, column=3, rowspan=2, sticky=N + S + W + E)
-        self.chatSubmitBtn.grid(row=5, column=4, rowspan=2, sticky=N + S + W + E)
+        self.chatBox.grid(row=1, column=3, rowspan=6, columnspan=2, sticky=N + S)
+        self.chatInput.grid(row=7, column=3, rowspan=2, sticky=N + S + W + E)
+        self.chatSubmitBtn.grid(row=7, column=4, rowspan=2, sticky=N + S + W + E)
         self.createChatRoomBtn.grid(row=5, columnspan=2, sticky=W + E)
         self.createGroupChatBtn.grid(row=6, columnspan=2, sticky=W + E)
         self.logoutBtn.grid(row=0, column=4, sticky=E)
+        self.enterPrivateChatBtn.grid(row=7, columnspan=2, sticky=W +E)
+        self.enterGlobalChatBtn.grid(row=8, columnspan=2, sticky=W + E)
 
         # config and binds
         # self.chatBox.config(state = DISABLED)
-        self.master.bind('<Return>', self.submitMessage)
-        self.chatSubmitBtn.bind('<Button-1>', self.submitMessage)
+        self.master.bind('<Return>', self.submitMessageGlobalChat)
+        self.chatSubmitBtn.bind('<Button-1>', self.submitMessageGlobalChat)
+        self.listboxUsers.bind("<<ListboxSelect>>", self.chatPrivateUser);
+        self.enterPrivateChatBtn.config(state=DISABLED)
+        self.enterGlobalChatBtn.bind('<Button-1>', self.initGlobalChatMode)
+
 
         # server stuff
         self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -105,10 +113,37 @@ class MainWindow:
         self.rT.start()
 
         self.s.sendto(self.usernameString, server)
-	self.s.sendto("has joined the Global Chat!", server)
+        self.s.sendto("has joined the Global Chat!", server)
 
-    def submitMessage(self, event):
-        print("submitMessage function accessed")
+    def initPrivateChatMode(self, event):
+        self.chatBox.delete(1.0, END) #clear text area
+        self.chatBox.insert(INSERT, "Welcome to Private Chat Mode!\n")
+        self.chatSubmitBtn.bind('<Button-1>', self.submitMessagePrivateChat)
+        self.master.bind('<Return>', self.submitMessagePrivateChat)
+
+    def initGlobalChatMode(self, event):
+        self.chatBox.delete(1.0, END) #clear text area
+        self.chatBox.insert(INSERT, "Welcome to Global Chat Mode!\n")
+        self.chatSubmitBtn.bind('<Button-1>', self.submitMessageGlobalChat)
+        self.master.bind('<Return>', self.submitMessageGlobalChat)
+
+    def chatPrivateUser(self, event):
+        self.enterPrivateChatBtn.config(state=NORMAL)
+        self.enterPrivateChatBtn.bind("<Button-1>", self.initPrivateChatMode)
+        self.chatRecipientNum = self.listboxUsers.curselection()[0] # get selected User's index in then convert to int
+        self.welcomeUserMessage.configure(text = "You are chatting with " + str(self.chatRecipientNum))
+        print(self.chatRecipientNum)
+
+    def submitMessagePrivateChat(self, event):
+        print("submitMessagePrivatChat function accessed")
+        message = self.chatInput.get()
+        self.chatBox.insert(INSERT, self.usernameString + ": " + message +"\n")
+        self.s.sendto(str(self.chatRecipientNum) + "pm" + self.usernameString + ": " + message, server)
+        print(self.usernameString + ": " + message, server)
+        self.chatInput.delete(0, 'end')
+
+    def submitMessageGlobalChat(self, event):
+        print("submitMessageGlobalChat function accessed")
         message = self.chatInput.get()
         self.s.sendto(self.usernameString + ": " + message, server)
         print(self.usernameString + ": " + message, server)
